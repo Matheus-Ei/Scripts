021d1b1267e7cb25012c65aada1cea314b6c5183
Matheus Eickhoff <127603510+Matheus-Ei@users.noreply.github.com>
2024-02-23 17:40:55 -0300

    Create activate_office.bat

---
+++ activate_office.bat
@@ -0,0 +1,9 @@
+REM ir onde na pasta onde o office esta instalado e ir na pasta office dentro dela
+for /f %x in ('dir /b ..\root\Licenses16\proplusvl_kms*.xrm-ms') do cscript ospp.vbs /inslic:"..\root\Licenses16\%x"
+cscript ospp.vbs /inpkey:XQNVK-8JYDB-WJ9W3-YJ8YR-WFG99
+cscript ospp.vbs /unpkey:BTDRB >nul
+cscript ospp.vbs /unpkey:KHGM9 >nul
+cscript ospp.vbs /unpkey:CPQVG >nul
+cscript ospp.vbs /sethst:e8.us.to
+cscript ospp.vbs /setprt:1688
+cscript ospp.vbs /act
